91f2df2799b9f18f1cc705de96a9e1f28fa57e1c
Ejal de Klerk <edeklerk@gmail.com>
2021-12-03 17:25:50 +0100

    day 2 2020

--- 2020/day2.ps1
+++ 2020/day2.ps1
@@ -2,12 +2,16 @@ $passwords = Get-Content 'inputs/day2.txt'
 
 foreach ($line in $passwords) {
     $req, $password = $line -split ': '
-    $min, $rest = $req -split '-'
-    $max, $letter = $rest -split ' '
+    $a, $rest = $req -split '-'
+    $b, $letter = $rest -split ' '
     $count = ($password.ToCharArray() | Where-Object {$_ -eq $letter} | Measure-Object).Count
-    if ($count -ge $min -and $count -le $max) {
-        $counter++
+    if ($count -ge $a -and $count -le $b) {
+        $part1++
+    }
+    if ($password[$a-1] -eq $letter -xor $password[$b-1] -eq $letter) {
+        $part2++
     }
 }
 
-'The answer to part 1 is: ' + $counter
+'The answer to part 1 is: ' + $part1
+'The answer to part 2 is: ' + $part2
