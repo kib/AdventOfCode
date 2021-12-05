0f90b7f92e95e3ea82f569cf9ce8da8a0962efed
Ejal de Klerk <edeklerk@gmail.com>
2021-12-05 23:58:46 +0100

    AutoFormat

--- 2020/day5.ps1
+++ 2020/day5.ps1
@@ -1,12 +1,12 @@
 $bps = Get-Content -Path './inputs/day5.txt'
 
 $IDs = foreach ($bp in $bps) {
-    $row = [Convert]::toInt32(($bp.subString(0,7) -replace 'F','0' -replace 'B','1'),2)
-    $seat = [Convert]::ToInt32(($bp.subString(7,3) -replace 'L','0' -replace 'R','1'), 2)
-    $row*8+$seat
+    $row = [Convert]::toInt32(($bp.subString(0, 7) -replace 'F', '0' -replace 'B', '1'), 2)
+    $seat = [Convert]::ToInt32(($bp.subString(7, 3) -replace 'L', '0' -replace 'R', '1'), 2)
+    $row * 8 + $seat
 }
 
-$max = $IDs | Sort-Object | Select -Last 1
+$max = $IDs | Sort-Object | Select-Object -Last 1
 "The answer to part 1 is: $max"
 
 $started = $false
@@ -15,10 +15,9 @@ $IDs | Sort-Object | Foreach-Object {
     if (-not $started) {
         $started = $True
     }
-    elseif ($_ -ne $previous+1) {
-        "The answer to part 2 is: " + ($previous + 1)
+    elseif ($_ -ne $previous + 1) {
+        'The answer to part 2 is: ' + ($previous + 1)
         break
     }
     $previous = $_
-
 }
