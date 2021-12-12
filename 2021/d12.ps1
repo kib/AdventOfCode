4a7073a9c7644bac20cde90f2efb80d35df1bbcc
Ejal de Klerk <edeklerk@gmail.com>
2021-12-12 14:29:14 +0100

    Aoc 2021 day 12 part 1 + 2 commented

--- 2021/d12.ps1
+++ 2021/d12.ps1
@@ -1,16 +1,16 @@
-$data = Get-Content -Path './inputs/d12.txt'
+$data = Get-Content -Path './inputs/d12_demo3.txt'
 $target = @{}
-$paths = @{}
-$smallvisited = {}
-$counter = 0
+# $paths = @{}
+# $process = 0
+$pc = 0
 
 foreach ($line in $data) {
-    $id, $d = $line -split '-'
-    if (-not ($d -in $target.$id)) {
-        $target.$id += , $d
+    $a, $b = $line -split '-'
+    if (-not ($a -in $target.$b) -and ($a -ne 'start')) {
+        $target.$b += , $a
     }
-    if (-not ($id -in $target.$d)) {
-        $target.$d += , $id
+    if (-not ($b -in $target.$a) -and ($b -ne 'start')) {
+        $target.$a += , $b
     }
     $target.Remove('end')
 }
@@ -19,51 +19,40 @@ function get-Paths {
     param (
         [String[]] $p = @(),
         [String[]] $u = @(),
+        [String] $u2 = '',
         [int] $id
     )
-    if ($p.Count -eq 0) {
-        $p = ,'start'
-    }
+    # Write-Verbose "[$id] Branched from '$($p -join '-')' with [$($u -join ',')] and [$u2]"
     $last = $p[-1]
-    #comm
-    $key = $p -join '-'
-    $l = $u -join ','
-    Write-Verbose "[$id] Branched from '$key' with '$l' already visited"
-    Write-Verbose "[$id] Last item was '$last'"
-    #/comm
     $poss = [String[]]$target.$last
     foreach ($t in $poss) {
-        $nu = $u
-        $key2 = $p -join '-'
-        Write-Verbose "[$id] Working on '$key2"
-        Write-Verbose "[$id] Working on '$key'"
-        Write-Verbose "[$id] -> '$t' (from '$($poss -join ",")'), visited '$($u -join ',')'"
-        # special case
-        if ($t -eq 'start') {
-            continue
-        }
-        #end of path
+        # $script:process++
+        # Write-Verbose "[$id] '$($p -join '-')-<$t>' from [$($poss -join "|")] with [$($u -join ',')] and [$u2]"
         if ($t -eq 'end') {
-            $end = $p + $t
-            $key = $end -join ','
-            Write-Verbose "[$id] FOUND: $key"
-            $paths.$key = $end
+            # $key = $($p + $t) -join ','
+            # $paths.$key = $($p + $t)
+            $script:pc ++
+            # Write-Verbose "[$id] FOUND $key"
             continue
         }
-        # can't visit small cave twice
+        $nu = $u
+        $nu2 = $u2
         if ([int][char]$t[0] -gt 90) {
-            if ($t -in $u) {
-                continue
+            if ($t -in $nu) {
+                if ('' -eq $nu2) {
+                    $nu2 = $t
+                } else {
+                    continue
+                }
             } else {
                 $nu = $nu + $t
             }
         }
-        # if we made it here we can branch
-        get-Paths $($p + $t) $nu $($id+1)
+        get-Paths $($p + $t) $nu $nu2 $($id+1)
     }
 }
+get-Paths 'start'
 
-get-Paths
-
-Write-verbose ($paths.Keys -join '`n')
-"The answer to part 1 is: {0}" -f $paths.Keys.Count
+# $paths.Keys | Sort-Object
+"The answer to part 2 is: {0}" -f $pc
+# "This took $process steps"
---
+++ 2021/d12_part2.ps1
@@ -0,0 +1,58 @@
+    $data = Get-Content -Path './inputs/d12.txt'
+    $target = @{}
+    # $paths = @{}
+    # $process = 0
+    $pc = 0
+
+    foreach ($line in $data) {
+        $a, $b = $line -split '-'
+        if (-not ($a -in $target.$b) -and ($a -ne 'start')) {
+            $target.$b += , $a
+        }
+        if (-not ($b -in $target.$a) -and ($b -ne 'start')) {
+            $target.$a += , $b
+        }
+        $target.Remove('end')
+    }
+
+    function get-Paths {
+        param (
+            [String[]] $p = @(),
+            [String[]] $u = @(),
+            [String] $u2 = '',
+            [int] $id
+        )
+        # Write-Verbose "[$id] Branched from '$($p -join '-')' with [$($u -join ',')] and [$u2]"
+        $last = $p[-1]
+        $poss = [String[]]$target.$last
+        foreach ($t in $poss) {
+            # $script:process++
+            # Write-Verbose "[$id] '$($p -join '-')-<$t>' from [$($poss -join "|")] with [$($u -join ',')] and [$u2]"
+            if ($t -eq 'end') {
+                # $key = $($p + $t) -join ','
+                # $paths.$key = $($p + $t)
+                # Write-Verbose "[$id] FOUND $key"
+                $script:pc ++
+                continue
+            }
+            $nu = $u
+            $nu2 = $u2
+            if ([int][char]$t[0] -gt 90) {
+                if ($t -in $nu) {
+                    if ('' -eq $nu2) {
+                        $nu2 = $t
+                    } else {
+                        continue
+                    }
+                } else {
+                    $nu = $nu + $t
+                }
+            }
+            get-Paths $($p + $t) $nu $nu2 $($id+1)
+        }
+    }
+    get-Paths 'start'
+
+    # $paths.Keys | Sort-Object
+    "The answer to part 2 is: {0}" -f $pc
+    # "This took $process steps"
