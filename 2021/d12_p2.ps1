5e521d0dead0b2e80c05285c8a3c4f5bffc9db8f
Ejal de Klerk <edeklerk@gmail.com>
2021-12-12 21:56:57 +0100

    commented out all code purely for debugging

--- 2021/d12_p2.ps1
+++ 2021/d12_p2.ps1
@@ -1,7 +1,7 @@
     $data = Get-Content -Path './inputs/d12_demo1.txt'
     $targets = @{}
-    $paths = @{}
-    $process = 0
+    # $paths = @{}
+    # $process = 0
     $pathCount = 0
 
     foreach ($line in $data) {
@@ -22,20 +22,19 @@
             [String] $twiceCave = '',
             [int] $id
         )
-        Write-Verbose "[$id] Branched from '$($pathTaken -join '-')' with [$($smallCaves -join ',')] small visited ([$twiceCave] twice)"
+        # Write-Verbose "[$id] Branched from '$($pathTaken -join '-')' with [$($smallCaves -join ',')] small visited ([$twiceCave] twice)"
         $last = $pathTaken[-1]
         $possibleNodes = [String[]]$targets.$last
         foreach ($node in $possibleNodes) {
             $script:process++
-            Write-Verbose "[$id] '$($pathTaken -join '-')-<$node>' from [$($possibleNodes -join "|")] with small [$($smallCaves -join ',')] used ([$twiceCave] twice)"
+            # Write-Verbose "[$id] '$($pathTaken -join '-')-<$node>' from [$($possibleNodes -join "|")] with small [$($smallCaves -join ',')] used ([$twiceCave] twice)"
             if ($node -eq 'end') {
-                $key = $($pathTaken + $node) -join ','
-                $paths.$key = $($pathTaken + $node)
-                Write-Verbose "[$id] FOUND $key"
+                # $key = $($pathTaken + $node) -join ','
+                # $paths.$key = $($pathTaken + $node)
+                # Write-Verbose "[$id] FOUND $key"
                 $script:pathCount ++
                 continue
             }
-            # we do not want to change these inside the loop, but only when we recurse
             $smallCaveCopy = $smallCaves
             $twiceCaveCopy = $twiceCave
             if ([int][char]$node[0] -gt 90) {
@@ -54,6 +53,6 @@
     }
     get-Paths 'start'
 
-    $paths.Keys | Sort-Object
+    # $paths.Keys | Sort-Object
     "The answer to part 2 is: {0}" -f $pathCount
-    "This took $process steps"
+    # "This took $process steps"
